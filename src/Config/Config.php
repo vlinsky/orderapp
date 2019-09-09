29768d377ea901bbf6927d6b5514b02a9c6f2fd6
Vlad <vlad.linsky@gmail.com>
2019-09-09 16:47:18 +0300

    Config class added

---
+++ src/Config/Config.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Config;
+
+/**
+ * App Config class
+ * 
+ * get data from .env file
+ */
+class Config
+{
+    /**
+     * get redis connection string
+     * @return string
+     */
+    public static function getRedisSocket() : string
+    {
+        return $_ENV['REDIS_CONNECTION_STRING'];
+    }
+    
+    /**
+     * get redis session expire time from .env
+     * @return string
+     */
+    public static function getRedisSessionTimeout() : string
+    {
+        return $_ENV['REDIS_SESSION_TIMEOUT'];
+    }
+    
+    /**
+     * get redis session namespace from .env
+     * @return string
+     */
+    public static function getRedisSessionNamespace() : string
+    {
+        return $_ENV['REDIS_SESSION_NEMASPACE'];
+    }   
+}
