d55cead7d9e3359a5c11180bf792586df9714e3f
Vlad <vlad.linsky@gmail.com>
2019-09-10 11:50:09 +0300

    added static class for response status and error msg

---
+++ src/Config/Error.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Config;
+
+/**
+ * Static class that contains default errors message
+ */
+class Error
+{
+    public const INTERNAL_ERROR = 'internal error';
+    public const CANNOT_CREATE_ORDER_ERROR = 'cannot crate order';
+}
---
+++ src/Config/ResponseStatus.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Config;
+
+/**
+ * ResponseStatus
+ * 
+ * contain const for response status  
+ */
+class ResponseStatus
+{
+    public const STATUS_OK = 'OK';
+    public const STATUS_ERROR = 'ERROR';
+}
